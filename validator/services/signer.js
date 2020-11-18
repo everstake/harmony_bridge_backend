0c2d2a8ab789aef0a0134ee47d50c177fa6596f6
Vadim <vadim.p@blaize.tech>
2020-11-18 17:32:52 +0200

    fix signer

--- validator/services/signer.js
+++ validator/services/signer.js
@@ -2,6 +2,6 @@ const ethSig = require("../nano-ethereum-signer");
 const keys = require("../config/keys.json");
 
 exports.signMessageForHarmony = function (swapMessage) {
-  let signature = ethSig.signMessage(swapMessage, this.keys.harmony_key);
+  let signature = ethSig.signMessage(swapMessage, keys.harmony_key);
   return signature;
 };
