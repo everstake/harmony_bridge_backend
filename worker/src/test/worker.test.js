87008f831426f6bcc7253a82cbcfb1a39cf3f6f8
spreadzp <spread2009@gmail.com>
2021-05-17 17:38:01 +0300

    set localhost

--- worker/src/test/worker.test.js
+++ worker/src/test/worker.test.js
@@ -30,7 +30,7 @@ global.gConfig = {
     },
     "polka": {
         "chain_id": 1,
-        "endpoint": "ws://0.0.0.0",
+        "endpoint": "ws://localhost",
         "contractAddress": "5GVy4KCvf1p4hcyk3rEvBHt3oGCcvFFzZez3NVqthkmoFEQq",
         "signatureThreshold": 2,
         "validators": [ "test-validator1", "test-validator2", "test-validator3" ]
